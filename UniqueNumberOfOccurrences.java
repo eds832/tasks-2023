87f6f10a11a4ec6b435aafadd98ce50bd12b5dd4
eds832 <eds832@gmail.com>
2023-08-15 21:37:06 +0200

    Update UniqueNumberOfOccurrences.java with 1ms record

--- UniqueNumberOfOccurrences.java
+++ UniqueNumberOfOccurrences.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 /**
  * @author Eduard_Sardyka
  */
@@ -12,6 +9,7 @@ class UniqueNumberOfOccurrences {
 
     static class Solution {
         /**
+         * https://leetcode.com/problems/unique-number-of-occurrences/
          * Given an array of integers arr, return true if the number
          * of occurrences of each value in the array is unique or false otherwise.
          * Example 1:
@@ -22,19 +20,38 @@ class UniqueNumberOfOccurrences {
          * -1000 <= arr[i] <= 1000
          */
         public boolean uniqueOccurrences(int[] arr) {
-            Map<Integer, Integer> map = new HashMap<>(arr.length);
-            ArrayList<Integer> list = new ArrayList<>(arr.length);
-            for (int i : arr) {
-                map.merge(i, 1, (oldValue, newValue) -> oldValue + newValue);
-            }
-            for (Integer i : map.keySet()) {
-                if (!list.contains(map.get(i))) {
-                    list.add(map.get(i));
+            int[] keys = new int[arr.length];
+            int[] values = new int[arr.length];
+            int maxIndex = -1;
+            for(int val: arr) {
+                int index = indexOf(val, keys, maxIndex);
+                if(index < 0) {
+                    maxIndex++;
+                    keys[maxIndex] = val;
+                    values[maxIndex] = 1;
                 } else {
+                    values[index] = values[index] + 1;
+                }
+            }
+            int[] check = new int[maxIndex + 1];
+            int maxCheck = -1;
+            for(int i = 0; i <= maxIndex; i++) {
+                int val = values[i];
+                if(indexOf(val, check, maxCheck) >= 0) {
                     return false;
+                } else {
+                    maxCheck++;
+                    check[maxCheck] = val;
                 }
             }
             return true;
+        }
+
+        public static int indexOf(int val, int[] arr, int maxIndex) {
+            for (int i = 0; i <= maxIndex; i++) {
+                if (val == arr[i]) return i;
+            }
+            return -1;
         }
     }
 
